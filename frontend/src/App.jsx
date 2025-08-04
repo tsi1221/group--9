d56865f057e51e20777e9897a8c9749a9d04a443
tsi1221 <tsehayneshbiruh2@gmail.com>
2025-08-04 12:20:48 +0300

    new folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -11,14 +9,12 @@ function App() {
       <div>
       <h1>i am a winner always what ever when ever</h1>
       </div>
-      <h1>Vite + React</h1>
+     
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
@@ -28,3 +24,4 @@ function App() {
 }
 
 export default App
+// Edited by Tsehay on August 4
