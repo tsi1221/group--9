0fe7068d6b547fb051161ae587bfe56deb9fc8bf
Alazar-io <lazarus4142@gmail.com>
2025-08-16 20:58:28 -0800

    modification of singin and  signup

--- frontend/src/pages/public/Signin/Signin.jsx
+++ frontend/src/pages/public/Signin/Signin.jsx
@@ -15,7 +15,7 @@ const Signin = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('/api/login', {
+      const res = await fetch('http://localhost:3000/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
